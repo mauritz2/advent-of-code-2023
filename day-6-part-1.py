98a60ddd63528b7caeea72df5368634ef1a63f23
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-12-10 14:53:43 +0100

    day 6 part 1

--- day-6-part-1.py
+++ day-6-part-1.py
@@ -1,16 +1,24 @@
+import math 
+
 with open("day-6-data.txt") as f:
     data = f.readlines()
     time = list(map(int, data[0].strip().split()[1:]))
     distance = list(map(int, data[1].strip().split()[1:]))
 
-total_won_races = 0
+total_ways_to_win = []
+
 for (race_time, record) in zip(time, distance):
-    print(race_time)
-    print(record)
+    ways_to_win_race = 0
     
-    for i in range(0, time + 1):
+    for i in range(0, race_time + 1):
+        # 8
+        # 
         speed = i
-        distance = speed * (time - i)
+        distance = speed * (race_time - i)
         if distance > record:
-            total_won_races += 1
-print(total_won_races)
+            print(distance)
+            ways_to_win_race += 1
+
+    total_ways_to_win.append(ways_to_win_race)
+    
+print(math.prod(total_ways_to_win))

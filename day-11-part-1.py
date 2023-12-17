181ccfbbc090e2109aebd06bafc68b7a7f139d50
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-12-17 18:41:42 +0100

    day 11 part 1

--- day-11-part-1.py
+++ day-11-part-1.py
@@ -1,45 +1,26 @@
 from itertools import combinations
 
 def expand_rows(universe:list[list[str]]) -> list[list[str]]:
-    expanded_universe = []
-    width = len(universe[0])
-
-    for row_data in universe:
-        if len([char for char in row_data if char != "."]) == 0:
-            expanded_universe.append("." * width)
-        expanded_universe.append(row_data)
-
-    return expanded_universe
+    row_to_expand = [i for i, col in enumerate(universe) if all(cell == "." for cell in col)]
+    row_to_expand.reverse()
+    for row_num in row_to_expand:
+        universe.insert(row_num, "." * len(universe[0]))
+    return universe
 
 def expand_cols(universe:list[list[str]]) -> list[list[str]]:
-    width = len(universe[0])
-
-    # Transpose the matrix to work with rows instead of columns
-    #transposed_universe = list(map(list, zip(*universe)))
-
-    #expanded_universe_t = expand_rows(transposed_universe)
-
-
-    col_nums_to_expand = []
-    for col_num in range(width):
-        if all([row[col_num] == "." for row in universe]):
-            col_nums_to_expand.append(col_num)
-
-    col_nums_to_expand.reverse()
-
-    for col_num in col_nums_to_expand:
-        for i, row in enumerate(universe):
-            universe[i] = row[:col_num] + "X" + row[col_num:]
-    return universe
+    universe_t = ["".join(row) for row in zip(*universe)]
+    expanded_universe_t = expand_rows(universe_t)
+    expanded_universe = ["".join(row) for row in zip(*expanded_universe_t)]
+    return expanded_universe
 
 def find_all_galaxies(universe: list[list[str]]) -> dict[int, tuple[int]]:
     galaxy_locs = {}
     galaxy_num = 1
     for y, row_data in enumerate(universe):
-        last_x = 0
-        while row_data.find("#", last_x) != -1:
-            x = row_data.find("#", last_x)
-            last_x = x + 1
+        next_x_search_start = 0
+        while row_data.find("#", next_x_search_start) != -1:
+            x = row_data.find("#", next_x_search_start)
+            next_x_search_start = x + 1
             galaxy_locs[galaxy_num] = (y, x)
             galaxy_num += 1
     return galaxy_locs
@@ -52,27 +33,22 @@ def get_universe():
         data = expand_cols(data)
     return data
 
-def get_differences(galaxy_pairs, galaxies):
+def get_shortest_distance(galaxy_pairs, galaxies):
     differences = 0
     for g_1, g_2 in galaxy_pairs:
         g_1_loc = galaxies[g_1]
         g_2_loc = galaxies[g_2]
-
         y_diff = abs(g_1_loc[0] - g_2_loc[0])
         x_diff = abs(g_1_loc[1] - g_2_loc[1])
         total_diff = x_diff + y_diff
-        print(f"For ({(g_1, g_2)} the difference is {total_diff}")
         differences += total_diff
     return differences
 
 def main():
     universe = get_universe()
-    for row in universe:
-        print(row)
     galaxies = find_all_galaxies(universe)
     galaxy_pairs = list(combinations(galaxies, 2))
-    print(get_differences(galaxy_pairs, galaxies))
+    print(get_shortest_distance(galaxy_pairs, galaxies))
     print(galaxies)
-
 
 main()

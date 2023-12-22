b25012e9e10e367e737bb85e8affbda06978c197
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-12-22 23:05:31 +0100

    day 14 part 1

---
+++ day-14-part-1.py
@@ -0,0 +1,24 @@
+with open("day-14-data.txt") as f:
+    data = f.readlines()
+    data = [row.strip() for row in data]
+
+total_weight = 0
+row_len = len(data)
+
+for row_num, row in enumerate(data):
+    for col_num in range(len(data[0])):
+        if data[row_num][col_num] != "O":
+            continue
+        northern_positions = [pos[col_num] for pos in data[:row_num]][::-1]
+        northernmost_pos = row_num
+        if northern_positions:
+            for pos in northern_positions:
+                if pos == ".":
+                    northernmost_pos -= 1
+                else:
+                     break
+        data[row_num] = data[row_num][:col_num] + "." + data[row_num][col_num + 1:]
+        data[northernmost_pos] = data[northernmost_pos][:col_num] + "O" + data[northernmost_pos][col_num + 1:]
+        total_weight += (row_len - northernmost_pos)
+        
+print(total_weight)

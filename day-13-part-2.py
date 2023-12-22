970cb568bea7f47a7901cd04d4fb1213a7329826
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-12-22 22:33:23 +0100

    day 1 part 2

--- day-13-part-2.py
+++ day-13-part-2.py
@@ -1,38 +1,27 @@
 with open("day-13-data.txt") as f:
     all_terrains = []
-    terrain = [] 
-    data = f.readlines()
-    data = [row.strip() for row in data]
-
-    for row in data:
-        # Requires adding two spaces to the data so all terrains are captured
-        if len(row) == 0:
+    terrain = []
+    for row in f:
+        row = row.strip()
+        if row:
+            terrain.append(row)
+        else:
             all_terrains.append(terrain)
             terrain = []
-        else:
-            terrain.append(row)
+    if terrain:
+        all_terrains.append(terrain)
 
 def is_imperfect_mirror(above, below):
-    smudge_matches = 0
     perfect_matches = 0
+    smudge_matches = 0
+
     for a, b in zip(above, below):
         if a == b:
             perfect_matches += 1
-            continue
-        non_matching_chars = 0
-        for char1, char2 in zip(a, b):
-            if char1 != char2:
-                non_matching_chars += 1
-            if non_matching_chars > 1:
-                break
-        else:
-            if non_matching_chars == 1:
-                smudge_matches += 1
+        elif sum(char1 != char2 for char1, char2 in zip(a, b)) == 1:
+            smudge_matches += 1
 
-    if smudge_matches == 1 and perfect_matches == len(above) - 1:
-        return True
-    else:
-        return False
+    return smudge_matches == 1 and perfect_matches == len(above) - 1
 
 def find_horizontal_mirror_pos(terrain):
     row_len = len(terrain)
@@ -40,17 +29,11 @@ def find_horizontal_mirror_pos(terrain):
         rows_above = i
         rows_below = row_len - rows_above
         max_mirror_length = min(rows_below, rows_above)
-
         above = terrain[i - max_mirror_length:i]
-        below = terrain[i:i + max_mirror_length]
-        below.reverse()
-
+        below = terrain[i:i + max_mirror_length][::-1]
         if is_imperfect_mirror(above, below):
-            mirror_loc = i
-            return mirror_loc
+            return i
     return None
-
-
 
 total_sum = 0
 for terrain in all_terrains:
@@ -62,8 +45,3 @@ for terrain in all_terrains:
         mirror_loc = find_horizontal_mirror_pos(terrain_t)
         total_sum += mirror_loc
 print(total_sum)
-
-
-# Find the smudge
-# Correct the smudge
-# Find the new line

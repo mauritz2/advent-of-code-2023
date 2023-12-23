af2a3954aa17ba0f72d67e1563c5831690f7078b
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-12-23 15:05:36 +0100

    day 14 part 2 WIP

--- day-14-delete.py
+++ day-14-delete.py
@@ -1,8 +1,9 @@
 import time
 from collections import defaultdict
+from functools import lru_cache
 
-blocks_by_col = defaultdict(list)
-blocks_by_row = defaultdict(list)
+blocks_by_col = defaultdict(set)
+blocks_by_row = defaultdict(set)
 
 with open("day-14-data.txt") as f:
     data = f.readlines()
@@ -20,10 +21,10 @@ with open("day-14-data.txt") as f:
                 square_stones.append((row_num, col_num))
 
         for square in square_stones:
-            blocks_by_col[square[1]].append(square[0])
-            blocks_by_row[square[0]].append(square[1])
+            blocks_by_col[square[1]].add(square[0])
+            blocks_by_row[square[0]].add(square[1])
 
-new_round_rocks = []
+new_round_stones = []
 
 dir_mod = {
     "North": {"comp": lambda block, cur_stone: block < cur_stone, "nearest": max, "offset": 1, "max_pos":0, "block_dict":blocks_by_col},
@@ -32,12 +33,14 @@ dir_mod = {
     "East": {"comp": lambda block, cur_stone: block > cur_stone, "nearest": min, "offset": -1, "max_pos":len(data[0]), "block_dict":blocks_by_row}
 }
 
-def tilt_board(round_stones, blocks, direction):
+
+#@lru_cache(maxsize=None)  
+def tilt_board(round_stones, direction):
     new_round_rocks = []
 
     for round_stone in round_stones:
-        blocks_on_col = blocks[round_stone[1]]
-        nearest_blocks = [square for square in blocks_on_col if dir_mod[direction]["comp"](square, round_stone[0])]
+        blocks = dir_mod[direction]["block_dict"][round_stone[1]]
+        nearest_blocks = [square for square in blocks if dir_mod[direction]["comp"](square, round_stone[0])]
 
         if nearest_blocks:
             nearest_free_space = dir_mod[direction]["nearest"](nearest_blocks) + dir_mod[direction]["offset"]
@@ -45,22 +48,22 @@ def tilt_board(round_stones, blocks, direction):
             nearest_free_space = dir_mod[direction]["max_pos"]
 
         new_pos = (nearest_free_space, round_stone[1])
-        blocks[round_stone[1]].append(nearest_free_space)
+        dir_mod[direction]["block_dict"][round_stone[1]].add(nearest_free_space)
         new_round_rocks.append(new_pos)
 
     return new_round_rocks
 
-new_round_rocks = round_stones
+new_round_stones = round_stones
 
-for _ in range(1000000000):
-    new_round_rocks = tilt_board(new_round_rocks, blocks_by_col, direction="North")
-    new_round_rocks = tilt_board(new_round_rocks, blocks_by_col, direction="West")
-    new_round_rocks = tilt_board(new_round_rocks, blocks_by_col, direction="South")
-    new_round_rocks = tilt_board(new_round_rocks, blocks_by_col, direction="East")
+for _ in range(1):
+    new_round_stones = tilt_board(new_round_stones, direction="North")
+    #new_round_rocks = tilt_board(new_round_rocks, blocks_by_col, direction="West")
+    #new_round_rocks = tilt_board(new_round_rocks, blocks_by_col, direction="South")
+    #new_round_rocks = tilt_board(new_round_rocks, blocks_by_col, direction="East")
     print(_)
 
 total_score = 0
-for stone in new_round_rocks:
+for stone in new_round_stones:
     total_score += (num_rows - stone[0])
 
 print(total_score)

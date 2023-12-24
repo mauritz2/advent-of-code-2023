7448b3b39b2d448d1a16bb625ccdce32e5336d7a
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-12-24 11:33:58 +0100

    A start implementation WIP

--- 17-1.py
+++ 17-1.py
@@ -1,51 +1,56 @@
-from itertools import combinations_with_replacement
+import heapq
+
+def a_star(grid):
+    def heuristic(a, b):
+        # Manhattan distance on a square grid
+        return abs(b[0] - a[0]) + abs(b[1] - a[1])
+
+    def neighbors(node):
+        dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        result = []
+        for d in dirs:
+            nx, ny = node[0] + d[0], node[1] + d[1]
+            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+                result.append((nx, ny))
+        return result
+
+    start, goal = (0, 0), (len(grid)-1, len(grid[0])-1)
+    frontier = [(0, start)]
+    came_from = {start: None}
+    cost_so_far = {start: 0}
+
+    while frontier:
+        # current_cost intentionally not used - used to retrieve the next most promising node, but not used afterwards
+        current_cost, current = heapq.heappop(frontier)
+        if current == goal:
+            break
+
+        for next in neighbors(current):
+            new_cost = cost_so_far[current] + grid[next[0]][next[1]]
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                # Add heurisstic to penalize paths that go further away from the end
+                priority = new_cost + heuristic(goal, next)
+                heapq.heappush(frontier, (priority, next))
+                came_from[next] = current
+
+    # Reconstruct path
+    current = goal
+    path = []
+    heat_loss = 0
+    while current != start:
+        path.append(current)
+        heat_loss += grid[current[1]][current[0]]
+        current = came_from[current]
+    path.append(start)
+    path.reverse()
+
+    return path, heat_loss
+
 
 with open("inputs/17.txt") as f:
-    data = f.readlines()
-    data = [[int(char) for char in row.strip()] for row in data]
+    grid = [[int(num) for num in row.strip()] for row in f.readlines()]
 
-directions = {
-    "up":(-1, 0),
-    "down":(1, 0),
-    "left":(0, -1),
-    "right":(0, 1)
-}
-opposites = {
-    "left":"right",
-    "right":"left",
-    "up":"down",
-    "down":"up"
-}
-direction = "right"
-position = (0,0)
-
-while position != (13, 13):
-    
-    allowed_directions = [d for d in directions.keys() if d != opposites[direction]]
-    # Does not allow for a u turn! Maybe not needed since it's redundant anyways
-    step_combinations = [[directions[s] for s in step] for step in list(combinations_with_replacement(allowed_directions, 3))]
-    #step_combinations = [[(0,1),(1,0),(0,1)]]
-
-    least_heat = 100
-    for steps in step_combinations:
-        heat_loss = 0 
-        valid_steps = True
-        position = (0, 0)
-        heats_taken = []
-        heat_at_step = None
-        for step in steps:
-            position = (step[0] + position[0], step[1] + position[1]) 
-            if (position[0] < 0 or position[0] >= len(data)) or (position[1] < 0 or position[1] >= len(data[0])):
-                valid_steps = False
-                break
-            heat_at_step = data[position[0]][position[1]]
-            heats_taken.append(heat_at_step)
-            heat_loss += heat_at_step
-        if valid_steps:
-            if (heat_loss < least_heat):
-                least_heat = heat_loss
-                best_end = 
-                print(heats_taken)
-                print(least_heat)
-                print("###")
-    print(least_heat)
+path, heat_loss = a_star(grid)
+print(path)
+print(heat_loss)

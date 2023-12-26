a0b051396f48f8a338ac96d6efee244c795973e6
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-12-26 19:16:50 +0100

    day 20

--- day20.py
+++ day20.py
@@ -1,147 +1,57 @@
-all_nodes = {}
-
-class Node:
-    def __init__(self):
-        self.signal = False # False = low, True = high
-        self.incoming_signal = None
-        self.incoming_signal_source = None
-        self.signals_sent = {True: 0, False: 0}
-
-    def send_signals_and_return_receiving(self):
-        # Need for instance
-        for node_name in self.output_node_names:
-            if node_name not in all_nodes.keys():
-                # E.g., output node doesn't exist
-                print(f"{node_name} doesn't exist!")
-                continue
-            node = all_nodes[node_name]
-            node.incoming_signal = self.signal
-            node.incoming_signal_source = self.name
-            node.process_incoming_signals()
-            self.signals_sent[self.signal] += 1
-        return self.output_node_names
+# By /u/4HbQ
+from collections import defaultdict
+from math import prod
 
 
-class Broadcaster(Node):
-    def __init__(self, name, output_node_names):
-        super().__init__()
-        self.name = name
-        self.output_node_names = output_node_names
-    
-    def process_incoming_signals(self):
-        pass
+modules = dict()
+flips = defaultdict(int)
+conjs = defaultdict(dict)
 
+for line in open('inputs/20.txt'):
+    s, _, *ds = line.replace(',', '').split()
+    t, s = (s[0], s[1:]) if s[0] in '%&' else ('', s)
 
-class FlipFlop(Node):
-    """
-    Flip-flop modules (prefix %) are either on or off; they are initially off. 
-    If a flip-flop module receives a high pulse, it is ignored and nothing happens. 
-    However, if a flip-flop module receives a low pulse, it flips between on and off. 
-    If it was off, it turns on and sends a high pulse. If it was on, it turns off and sends a low pulse.
-    """
-    def __init__(self, name, output_node_names):
-        self.output_node_names = output_node_names
-        self.name = name
-        self.is_on = False
-        super().__init__()
+    modules[s] = t, ds
 
-    def process_incoming_signals(self):
-        if self.incoming_signal is True:
-            self.signal = None
-        elif self.incoming_signal is False:
-            if self.is_on:
-                self.signal = False
-            else:
-                self.signal = True
-            self.is_on = not self.is_on
-        else:
-            raise ValueError("Incoming signal neither high nor low")
+    for d in ds:
+        conjs[d][s] = 0
+        if d == 'rx': rx = s
 
-    def send_signals_and_return_receiving(self):
-        if self.signal:
-            return super().send_signals_and_return_receiving()
-        else:
-            return []
+rx_ins = {i: 0 for i in conjs[rx]}
 
+presses = 0
+counts = [0, 0]
 
-class Conjunction(Node):
-    """ 
-    Conjunction modules (prefix &) remember the type of the most recent pulse received from each of their connected input modules; 
-    they initially default to remembering a low pulse for each input.
-    When a pulse is received, the conjunction module first updates its memory for that input.
-    Then, if it remembers high pulses for all inputs, 
-    it sends a low pulse; otherwise, it sends a high pulse.
-    """
-    def __init__(self, name, input_node_names, output_node_names):
-        self.name = name
-        self.input_node_states = self.create_node_states_dict(input_node_names)
-        self.output_node_names = output_node_names
-        super().__init__()
-    
-    def create_node_states_dict(self, input_node_names):
-        node_states = {name: False for name in input_node_names}
-        return node_states
+while True:
+    if presses == 1000:
+        print(prod(counts))
+    presses += 1
 
-    def is_all_input_high(self):
-        return all([node for node in self.input_node_states.values()])
-    
-    def process_incoming_signals(self):
-        self.input_node_states[self.incoming_signal_source] = self.incoming_signal
-        self.signal = self.is_all_input_high()
+    if all(rx_ins.values()):
+        print(prod(rx_ins.values()))
+        break
 
+    queue = [(None, 'broadcaster', 0)]
+    while queue:
+        source, mod, pulse_in = queue.pop(0)
+        counts[pulse_in] += 1
 
-class SignalProcessor:
-    def process_all_signals():
-        node_names_with_signals_to_send = ["broadcaster"]
-        all_nodes["broadcaster"].signals_sent[False] += 1 #button press
-        while node_names_with_signals_to_send:
-            next_node_name = node_names_with_signals_to_send.pop(0)
-            if next_node_name not in all_nodes.keys():
-                print(f"{next_node_name} doesn't exist")
-                continue
-            all_nodes[next_node_name].process_incoming_signals()
-            nodes_names_receiving_signals = all_nodes[next_node_name].send_signals_and_return_receiving()
-            # Why does b not send anything to c?
-            for new_node_name in nodes_names_receiving_signals:
-                node_names_with_signals_to_send.append(new_node_name)
-        # Remember the button press is 1!
+        if mod not in modules: continue
+        type, nexts = modules[mod]
 
-    def count_signals_sent():
-        low_sent = sum([node.signals_sent[0] for node in all_nodes.values()]) # 8
-        high_sent = sum([node.signals_sent[1] for node in all_nodes.values()]) # 4
-        return low_sent * high_sent
+        match type, pulse_in:
+            case '', _:
+                pulse_out = pulse_in
+            case '%', 0:
+                pulse_out = flips[mod] = not flips[mod]
+            case '&', _:
+                conjs[mod][source] = pulse_in
+                pulse_out = not all(conjs[mod].values())
 
-def get_name_and_nodes(row):
-    name, other = row.split("-")
-    name = name.strip()
-    nodes = other[1:].split(",")
-    nodes = [node.strip() for node in nodes]
-    return name, nodes
+                if 'rx' in nexts:
+                    for k, v in conjs[mod].items():
+                        if v: rx_ins[k] = presses
+            case _,_: continue
 
-with open("inputs/20.txt") as f:
-    data = f.readlines()
-    for row in data:
-        name, node_names = get_name_and_nodes(row)        
-        if "%" in name:
-            name = name[1:]
-            node = FlipFlop(name=name, output_node_names=node_names)
-        elif "&" in name:
-            name = name[1:]
-            conjunction_input_nodes = []
-            for row in data:
-                conj_input_name, conj_input_nodes = get_name_and_nodes(row)
-                if name in conj_input_nodes:
-                    conjunction_input_nodes.append(conj_input_name)
-            node = Conjunction(name=name, output_node_names=node_names, input_node_names=conjunction_input_nodes)
-        elif "caster" in name:
-            node = Broadcaster(name=name, output_node_names=node_names)
-        else:
-            raise ValueError("No match to other nodes")
-        all_nodes[name] = node
-
-
-#for _ in range(1000):
-SignalProcessor.process_all_signals()
-
-signals_sent = SignalProcessor.count_signals_sent()
-print(signals_sent)
+        for n in nexts:
+            queue.append((mod, n, pulse_out))

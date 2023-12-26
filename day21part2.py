125595ed49adeb8000e6fa3982e385004f317621
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-12-26 23:07:35 +0100

    Day 21

--- day21part2.py
+++ day21part2.py
@@ -1,75 +1,131 @@
-from functools import cache
+# From u/msschmitt
 
-with open("inputs/day21.txt") as f:
-    grid = f.readlines()
-    grid = [row.strip() for row in grid]
-    start_pos = None
-    for i, row in enumerate(grid):
-        s_pos = row.find("S")
-        if s_pos != -1:
-            start_pos = (i, s_pos)
-            break
-    grid = [[c for c in row] for row in grid]
-    grid[start_pos[0]][start_pos[1]] = "."
+#!/usr/bin/env python3
+# 2023 Day 21: Step Counter
 
-directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+from collections import deque
 
-# Check fewer locations
-    # Figure out which ones we don't have to compute - e.g., already visisted
-# Check each location faster
-    # More advanced caching, 
+def process_input(filename):
+    """Acquire input data"""
+    with open(filename) as file:
+        input = file.read().splitlines()
 
-def find_num_locations(start, max_steps):
-    current_positions = [start]
-    all_positions = {(start, 0)}
-    for _ in range(1, max_steps + 1):
-        new_positions = set()
-        while current_positions:
-            pos = current_positions.pop()
-            new_directions = get_new_directions(pos)
-            new_positions.update(new_directions)
-        else:
-            current_positions = new_positions
-    return current_positions
+    garden = {}
 
+    x_len = len(input[0])
+    y_len = len(input)
 
-@cache
-def shrink_to_original_map(pos):
-    mapped_y = pos[0] % len(grid)
-    mapped_x = pos[1] % len(grid[0])
-    return mapped_y, mapped_x
+    for y, line in enumerate(input):
+        for x, ch in enumerate(line):
+            garden[(x,y)] = ch
+            if ch == 'S':
+                start = (x,y)
 
-@cache
-def get_new_directions(pos):
-    mapped_y, mapped_x = shrink_to_original_map(pos)
-    available_directions = grid_map[(mapped_y, mapped_x)]
-    new_directions = set([(pos[0] + dir[0], pos[1] + dir[1]) for dir in available_directions])
-    return new_directions
+    return garden, start, x_len, y_len
 
-def get_grid_map(grid, directions):
-    grid_map = {}
-    for row_num, row in enumerate(grid):
-        for col_num, cell in enumerate(row):
-            if grid[row_num][col_num] == "#":
+def take_steps(step_limit):
+    stepped_to = set()
+    stepped_to.add(start)
+
+    for s in range(step_limit):
+        stepped_to = take_a_step(stepped_to)
+    return stepped_to
+
+def take_a_step(from_steps):
+    to_steps = set()
+    for pos in from_steps:
+        for dir in 'NWES':
+            new_pos = adjust_pos(pos, dir)
+            if garden_ch(new_pos) == '#':      # rock
                 continue
-            possible_directions = []
-            for dir in directions:
-                ny, nx = row_num + dir[0], col_num + dir[1]
-                if (0 <= ny < len(grid)) and (0 <= nx < len(grid[0])):
-                    if grid[ny][nx] != ".":
-                        continue
-                ny_normalized, nx_normalized = ny - row_num, nx - col_num
-                possible_directions.append((ny_normalized, nx_normalized))
+            to_steps.add(new_pos)
+    return to_steps
 
-            grid_map[(row_num, col_num)] = possible_directions
-    return grid_map
+def adjust_pos(coord, dir):
+    x, y = coord
+    adjust = {'E':(1,0), 'W':(-1,0), 'S':(0,1), 'N':(0,-1)}
+    ax, ay = adjust[dir]
+    x += ax
+    y += ay
+    return (x, y)
 
-
-grid_map = get_grid_map(grid, directions)
-
-#16 733 044
-
-locations = set(find_num_locations(start_pos, 50))
-print(len(locations))
+def garden_ch(coord):
+    x, y = coord
+    a = x % x_len
+    b = y % y_len
+    ch = garden[(a,b)]
+    return ch
 
 
+def analyze_result():
+    print('Analyzing for N =',n)
+    print('Total plots reached:',len(plots_reached))
+    min_x, min_y, max_x, max_y = 0, 0, 0, 0
+    for x, y in plots_reached:
+        min_x = min(min_x, x)
+        min_y = min(min_y, y)
+        max_x = max(max_x, x)
+        max_y = max(max_y, y)
+    squares = int((max_x - min_x + 1) / x_len)
+    print('Area reached is',squares,'x',squares)
+
+    square_totals = []
+    x = min_x
+    y = min_y
+    for a in range(squares):        # down
+        row = []
+        for b in range(squares):    # across
+            plots = 0
+            for x1 in range(x,x+x_len):
+                for y1 in range(y,y+y_len):
+                    if (x1, y1) in plots_reached:
+                        plots += 1
+            row.append(plots)
+            x += x_len
+        square_totals.append(row)
+        x = min_x
+        y += y_len
+    return square_totals
+
+
+def calculate_plots():
+    plots = 0
+
+    full_odd = squares[2][2]
+    full_even = squares[2][1]
+    s1 = squares[0][1] + squares[0][3] + squares[4][1] + squares[4][3]
+    s2 = squares[0][2] + squares[2][0] + squares[2][4] + squares[4][2]
+    s3 = squares[1][1] + squares[1][3] + squares[3][1] + squares[3][3]
+
+    plots = (s1 * n) + s2 + (s3 * (n-1)) + (full_even * n**2) + (full_odd * (n-1)**2)
+    return plots
+
+#-----------------------------------------------------------------------------------------
+
+filename = 'inputs/day21.txt'
+
+garden, start, x_len, y_len = process_input(filename)
+
+garden_width = x_len
+half_width = int((x_len - 1)/2)
+
+# Analyze results with N=2...
+print()
+print('Taking steps...')
+n = 2
+plots_reached = take_steps(half_width + garden_width * n)
+squares = analyze_result()
+
+print()
+for s in squares:
+    print(s)
+print()
+
+target_steps = 26501365
+n = int((target_steps - half_width) / garden_width)
+print('Answer requires N =',n)
+
+final_plots = calculate_plots()
+
+print()
+print('Plots reached: ', final_plots)
